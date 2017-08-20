d0b9b0f5b7cb50ef6063bcc22680f22ac46c4227
Rey Polanco <reypolanco@gmail.com>
2017-08-20 14:13:03 -0400

    feat: render Postlist Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import CategoriesList from './CategoriesList'
+import PostList from './PostsList'
 
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchCategories()
-    this.props.fetchComments()
     this.props.fetchPosts()
-
   }
   render() {
-    const { categories } = this.props
+    const { categories, posts } = this.props
     return (
       <div className="App">
         <CategoriesList categories={categories}/>
+        <PostList posts={posts}/>
       </div>
     );
   }
 }
 
 function mapStateToProps({ posts, categories, comments }) {
-  return{ posts, categories, comments }
+  return{ posts, categories }
 }
 
 export default connect(mapStateToProps, actions)(App);
