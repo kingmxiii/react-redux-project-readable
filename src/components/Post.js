fe287f98a0ed22161ca44c9a127b80b757cd77f8
Rey Polanco <reypolanco@gmail.com>
2017-08-22 21:40:14 -0400

    feat: eclose post title in a link to navigate to post details

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Post(props){
   const { post, comments } = props
   return (
     <div className="post-item">
       <div className="post-header">
-        <h3>{post.title}</h3>
+        <Link to={`/${post.category}/${post.id}`}><h3>{post.title}</h3></Link>
         <span>{post.author}</span>
       </div>
       <div className="post-tools">
