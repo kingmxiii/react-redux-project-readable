36ee0c669600cb9c2eb05a906f955e8533fefdf5
Rey Polanco <reypolanco@gmail.com>
2017-08-22 22:59:36 -0400

    feat: render post comment

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchPost } from '../actions'
+import { fetchPost, fetchComments } from '../actions'
+import _ from 'lodash'
 
 class PostDetails extends Component {
   componentDidMount() {
     const { post_id } = this.props.match.params
     this.props.fetchPost(post_id)
+    this.props.fetchComments(post_id)
   }
   render(){
-    const { post } = this.props
+    const { post, postComments } = this.props
     if(!post) {
       return <div>Loading...</div>;
     }
@@ -26,13 +28,28 @@ class PostDetails extends Component {
           <span className="post-author">{post.author}</span>
           <span className="post-date">{post.date}</span>
         </div>
+        <div className="post-comments">
+          <h3>Comments</h3>
+          {_.map(postComments,(comment) => {
+            return (
+              <div key={comment.id} className="comemnt-item">
+                <p className="comment-body">{comment.body}</p>
+                <span className="comment-author">{comment.author}</span>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
 }
 
-function mapStateToProps({posts}, ownProps) {
-  return { post: posts[ownProps.match.params.post_id] }
+function mapStateToProps({posts, comments }, ownProps) {
+  const { post_id } = ownProps.match.params
+  return {
+          post: posts[post_id],
+          postComments : comments[post_id]
+    }
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostDetails)
+export default connect(mapStateToProps, { fetchPost, fetchComments })(PostDetails)
