931001f2de3f06bab1e0c4bf9787e62bc814c73f
Rey Polanco <reypolanco@gmail.com>
2017-08-22 22:33:39 -0400

    feat: handle the case that url is accesed directly

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { fetchPost } from '../actions'
 
 class PostDetails extends Component {
-
+  componentDidMount() {
+    const { post_id } = this.props.match.params
+    this.props.fetchPost(post_id)
+  }
   render(){
     const { post } = this.props
+    if(!post) {
+      return <div>Loading...</div>;
+    }
     return(
+
       <div className="post-content">
         <h1 className="post-title">{post.title}</h1>
         <div className="post-toolbar">
-
+          {post.vote}
         </div>
         <div className="post-body">
           <p className="post-body-text">{post.body}</p>
@@ -27,4 +35,4 @@ function mapStateToProps({posts}, ownProps) {
   return { post: posts[ownProps.match.params.post_id] }
 }
 
-export default connect(mapStateToProps)(PostDetails)
+export default connect(mapStateToProps, {fetchPost})(PostDetails)
