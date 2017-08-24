2d84b3269377f9e144e6a9a28fb78671dcc4743d
Rey Polanco <reypolanco@gmail.com>
2017-08-23 22:44:04 -0400

    create action creator for post vote system

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 export const FETCH_CATEGORIES = 'FETCH_CATEGORIES'
 export const FETCH_COMMENTS = 'FETCH_COMMENTS'
+export const POST_VOTE = 'POST_VOTE'
 
 const ROOT_URL = 'http://localhost:5001'
 const  headers = { Authorization: 'reypolanco' }
@@ -42,4 +43,20 @@ export function fetchCategories(){
       dispatch({ type: FETCH_CATEGORIES, payload: data })
     })
   }
+}
+
+export function postVote(postId, option){
+  const request = axios({
+    url: `${ROOT_URL}/posts/${postId}`,
+    method : 'post',
+    headers,
+    data: { option }
+  }
+    )
+  return (dispatch) => {
+    request.then(({ data }) => {
+      dispatch({ type: POST_VOTE, payload: data })
+    })
+  }
+
 }
