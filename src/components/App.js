3ae61d597cdbe1a7b723e725e0a9f9420902014a
Rey Polanco <reypolanco@gmail.com>
2017-08-24 00:22:23 -0400

    feat create a form to add or edit Posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import CategoriesList from './CategoriesList'
 import PostList from './PostsList'
 import CategoryView from './CategoryView'
 import PostDetails from './PostDetails'
+import PostForm from './PostForm'
 
 
 class App extends Component {
@@ -19,14 +20,17 @@ class App extends Component {
       <BrowserRouter>
       <div className="App">
         <CategoriesList categories={categories}/>
-
+        <Link to="/post/action/new">New Post</Link>
+        <Link to="/post/action/edit/8xf0y6ziyjabvozdd253nd">New Post</Link>
 
         <div className="app-content">
         <Route exact path="/" render={ () => (
           <PostList posts={posts}/>
         )}/>
 
-        <Route path="/:category/:post_id" component={PostDetails}/>
+        <Route exact path="/post/action/:mode" component={PostForm}/>
+        <Route exact path="/post/action/:mode/:id" component={PostForm}/>
+        <Route exact path="/:category/:post_id" component={PostDetails}/>
 
         <Route exact path="/:category" render={ ( history ) =>{
           return (
---
+++ src/components/PostForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+
+class PostForm extends Component {
+  render(){
+    const  { categories } = this.props
+    return (
+    <div className="post-form-content">
+      <form>
+        <div>
+          <label>Title</label>
+          <div>
+            <Field
+            name="title"
+            component="input"
+            type="text"
+            placeholder="Post Title"
+            />
+          </div>
+        </div>
+        <div>
+          <label>Body</label>
+          <div>
+            <Field
+            name="body"
+            component="input"
+            type="textarea"
+            placeholder="Post Body"
+            />
+          </div>
+        </div>
+        <div>
+          <label>Title</label>
+          <div>
+            <Field
+            name="author"
+            component="input"
+            type="text"
+            placeholder="Post Author"
+            />
+          </div>
+        </div>
+        <div>
+          <label>Title</label>
+          <div>
+            <Field
+            name="title"
+            component="input"
+            type="text"
+            placeholder="Post Title"
+            />
+          </div>
+        </div>
+        <div>
+        <label>Category</label>
+        <div>
+          <Field name="category" component="select">
+            <option value="">Select a category...</option>
+            {categories.map(category =>
+              <option value={category.name} key={category.name}>
+                {category.name}
+              </option>
+            )}
+          </Field>
+        </div>
+      </div>
+      </form>
+    </div>
+  )
+  }
+}
+
+function mapStateToProps({ categories }){
+  return {
+    categories
+  }
+}
+
+export default reduxForm({
+  form: 'PostForm'
+}
+)(
+  connect(mapStateToProps)(PostForm)
+);
