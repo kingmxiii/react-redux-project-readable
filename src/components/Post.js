d9896e2c2c8d226dc2baa5d6faf1e2ea9ebc3d9a
Rey Polanco <reypolanco@gmail.com>
2017-09-03 18:47:30 -0400

    fix: convert post timestamp to date object to render the right date.

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,6 +5,7 @@ import PostMenu from './PostMenu'
 import Moment from 'react-moment'
 export default function Post(props){
   const { post, comments, deletePost } = props
+  const postTime = new Date(post.timestamp)
   return (
     <div className="post-item">
       <div className="post-handle row">
@@ -17,8 +18,8 @@ export default function Post(props){
           </Link>
           <span className="post-author">By: {post.author}</span>
           <span className="post-time">
-            <Moment unix format="MMMM DD hh:mm a">
-              {post.timestamp}
+            <Moment format="MMMM DD hh:mm a">
+              {postTime.toUTCString()}
             </Moment>
           </span>
         </div>
