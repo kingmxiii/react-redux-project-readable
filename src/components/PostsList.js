cf4c264fb2ad0ca3edbbe180837777b91227a7de
Rey Polanco <reypolanco@gmail.com>
2017-09-02 22:39:49 -0400

    feat: render post bar on postlist component

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { fetchComments, deletePost } from '../actions'
 import _ from 'lodash'
 import Post from './Post'
+import PostsBar from './PostsBar'
 
 class PostList extends Component {
 
@@ -15,6 +16,7 @@ class PostList extends Component {
     const { posts, comments, fetchComments, deletePost, history } = this.props
     return(
       <div className="posts-list">
+        <PostsBar/>
          { _.map(posts, post => {
            if (comments[post.id] === undefined){
               fetchComments(post.id)
