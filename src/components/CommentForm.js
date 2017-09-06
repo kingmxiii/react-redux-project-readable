a86cbf32e7b839f9c4353d53de6887cc96238d27
Rey Polanco <reypolanco@gmail.com>
2017-09-05 22:57:52 -0400

    feat: create commentform component

---
+++ src/components/CommentForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { Field, reduxForm, initialize } from 'redux-form'
+import { connect } from 'react-redux'
+import { createComment, updateComment } from '../actions'
+
+class CommentForm extends Component {
+
+  componentDidMount() {
+    const { mode } = this.props
+    if(mode === 'edit'){
+        this.initForm()
+    }
+  }
+
+  initForm(){
+    const { body } = this.props.comment
+    const formData = { body }
+    this.props.initialize(formData);
+  }
+
+  //Function to render fields dynamically
+  //Code from redux-form documentation
+  renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div>
+      <label>{label}</label>
+      <div>
+        <input {...input} placeholder={label} type={type}/>
+        {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+      </div>
+    </div>
+  )
+  onSubmit(values){
+    const { mode, id } = this.props.match.params
+    if(mode === 'new'){
+      values.id = Date.now().toString()
+      values.timestamp = Date.now()
+      this.props.createPost(values, () => {
+        this.props.history.push('/')
+      })
+    }
+    else{
+      this.props.updatePost(id, values, () => {
+        this.props.history.push('/')
+      })
+    }
+  }
+  render(){
+    const  { categories, handleSubmit } = this.props
+    const { mode } = this.props.match.params
+    return (
+
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <div>
+            <Field
+            name="owner"
+            label="Name"
+            component={this.renderField}
+            type="text"
+            />
+          </div>
+          <div>
+            <Field
+            name="body"
+            label="Comment"
+            component={this.renderField}
+            type="textarea"
+            />
+          </div>
+
+      <button type="submit" className="btn btn-primary">Save</button>
+      <button className="btn btn-danger">Cancel</button>
+      </form>
+  )
+  }
+}
+
+function mapStateToProps({ comments, form, appSettings }, ownProps){
+  const { parentId } = ownProps
+  const { commentId } = appSettings.commentModal
+  return{
+    comment: comments[parentId][commentId],
+    form
+  }
+}
+
+export default reduxForm({
+  form: 'CommentForm'
+}
+)(
+  connect(mapStateToProps, { createComment, updateComment })(CommentForm)
+);
