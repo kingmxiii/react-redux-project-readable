b2bcb1b52685e950b9f445ee95e6dba6a83434ca
Rey Polanco <reypolanco@gmail.com>
2017-09-06 00:20:33 -0400

    feat: set openModal as a handler for new Comment button

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -4,6 +4,7 @@ import { ButtonGroup, Button } from 'react-bootstrap'
 import Sort from './Sort'
 
 export default function MenuBar(props){
+  const { parentId, onNewClick } = props
   return(
     <div className="posts-bar">
       <ButtonGroup>
@@ -11,7 +12,14 @@ export default function MenuBar(props){
           <LinkContainer to="/post/action/new">
           <Button bsStyle="primary">New Post</Button>
         </LinkContainer> :
-          <Button bsStyle="primary">New Comment</Button>
+          <Button
+            bsStyle="primary"
+            onClick={() => {
+            onNewClick('new', null, parentId)
+          }}
+          >
+          New Comment
+        </Button>
        }
         <Sort entity={props.entity}/>
       </ButtonGroup>
