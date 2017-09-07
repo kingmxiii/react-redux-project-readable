73261024f6c8971fe5a30148173dcbfe72a65ac4
Rey Polanco <reypolanco@gmail.com>
2017-09-06 23:15:56 -0400

    feat: upgrade commentstat to a container

--- src/components/CommentStats.js
+++ src/components/CommentStats.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Vote from './Vote'
+import { connect } from 'react-redux'
+import { openModal } from '../actions'
 
-export default function CommentStats(props){
-  const { comment } = props
-  const commentDate = new Date(comment.timestamp)
-  return(
-    <div className="row comment-tools">
-      <div className="comment-actions pull-left">
-        <span className="pull-left">Edit</span>
-        <span className="pull-left">
-          <Vote postId={comment.id} />
-        </span>
-      </div>
-      <div className="comment-stats pull-left">
-          <span>Score: {comment.voteScore} </span>
-          <span>Posted: { commentDate.toUTCString() } </span>
-      </div>
-    </div>
-  )
+ class CommentStats extends Component {
+
+  render(){
+    const { comment } = this. props
+    const commentDate = new Date(comment.timestamp)
+    return(
+      <div className="row comment-tools">
+        <div className="comment-actions pull-left">
+          <span className="pull-left">Edit</span>
+          <span className="pull-left">
+            <Vote postId={comment.id} />
+          </span>
+        </div>
+        <div className="comment-stats pull-left">
+            <span>Score: {comment.voteScore} </span>
+            <span>Posted: { commentDate.toUTCString() } </span>
+          </div>
+        </div>
+      )
+  }
 }
+
+export default connect(null, { openModal })(CommentStats)
