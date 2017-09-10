f92ddb3a3b06cfa2a3283d1941c49811f562b8c7
Rey Polanco <reypolanco@gmail.com>
2017-09-10 18:38:41 -0400

    feat: make navbar bran a link to home page

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Navbar, Nav , NavItem, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import CategoriesList from './CategoriesList'
+import { Link } from 'react-router-dom'
 
 export default function AppMenu(props){
   return (
     <Navbar collapseOnSelect className="app-navbar">
       <Navbar.Header>
           <Navbar.Brand className="app-navbar-brand">
-            <span>My Readables</span>
+            <Link to="/">
+              <span>My Readables</span>
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle />
       </Navbar.Header>
