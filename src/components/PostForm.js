059a23afcf5f31c3c8851b81d3773e8568360d19
Rey Polanco <reypolanco@gmail.com>
2017-09-10 16:11:41 -0400

    feat: add form validation to post form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -8,28 +8,32 @@ class PostForm extends Component {
 
   componentDidMount() {
     const { mode } = this.props.match.params
-    if(mode === 'edit'){
-        this.initForm()
-    }
+    this.initForm(mode)
   }
 
-  initForm(){
-    const { title, body } = this.props.post
-    const formData = { title, body }
+  initForm(mode){
+    let formData;
+    if(mode === 'edit'){
+      const { title, body } = this.props.post
+      formData = { title, body }
+    }
+    else {
+      formData = { mode }
+    }
     this.props.initialize(formData);
   }
 
   //Function to render fields dynamically
   //Code from redux-form documentation
   renderField = ({ input, label, type, meta: { touched, error, warning } }) => {
-    const className = `form-group ${ touched & error ? 'has-danger' : ''}`
+    const className = `form-group ${ touched && error ? 'has-error' : ''}`
 
     return (
     <div className={ className }>
       <label>{label}</label>
       {(type === 'text') ?
-        <input {...input} placeholder={label} type={type} className="form-control" required />
-      : <textarea {...input} placeholder={label} type={type} className="form-control" rows="5" required ></textarea> }
+        <input {...input} placeholder={label} type={type} className="form-control" />
+      : <textarea {...input} placeholder={label} type={type} className="form-control" rows="5" ></textarea> }
 
       {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
     </div>
@@ -52,13 +56,20 @@ class PostForm extends Component {
     }
   }
   render(){
-    const  { categories, handleSubmit } = this.props
+    const  { categories, handleSubmit, pristine, submitting } = this.props
     const { mode } = this.props.match.params
     const formTitle = ( mode === 'new') ? 'New Post' : 'Edit Post'
     return (
       <div className="post-form-view">
           <h3>{formTitle}</h3>
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="post-form">
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="post-form" >
+
+            <Field
+              name='mode'
+              component='input'
+              type='hidden'
+              value='hello'
+            />
 
             <Field
             name="title"
@@ -95,15 +106,40 @@ class PostForm extends Component {
                   </option>
                 )}
               </Field>
+
             </div>
           }
 
-          <button type="submit" className="btn btn-primary">Save</button>
+          <button type="submit" className="btn btn-primary" disabled={ pristine || submitting }>Save</button>
           <Link to="/" className="btn btn-danger">Cancel</Link>
         </form>
       </div>
     )
   }
+}
+
+function validate(values){
+  const errors = {};
+  const { title, body, mode, author, category } = values
+  console.log(values)
+
+  if(!title){
+    errors.title = "Plesea enter a title!"
+  }
+
+  if(!body){
+    errors.body = "Post content cannot be empty!"
+  }
+
+  if( mode === 'new' && !author){
+    errors.author = "Please enter your Name!"
+  }
+
+  if( mode === 'new' && !category){
+    errors.category = "Please choose a category!"
+  }
+
+  return errors
 }
 
 function mapStateToProps({ posts, categories, form }, ownProps){
@@ -116,7 +152,8 @@ function mapStateToProps({ posts, categories, form }, ownProps){
 }
 
 export default reduxForm({
-  form: 'PostForm'
+  form: 'PostForm',
+  validate
 }
 )(
   connect(mapStateToProps, { createPost, updatePost })(PostForm)
